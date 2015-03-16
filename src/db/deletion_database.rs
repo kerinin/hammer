f162273edda7cc0557c0a4d51499b3e232655667
Ryan Michael <kerinin@gmail.com>
2015-03-16 08:00:57 -0500

    change soem stuff

--- src/db/deletion_database.rs
+++ src/db/deletion_database.rs
@@ -8,9 +8,9 @@ use self::num::rational::Ratio;
 use db::partition::Partition;
 //use db::result_accumulator::ResultAccumulator;
 use db::find_result::FindResult;
-use db::value::Value;
+use db::value::{Value, Window, DeletionVariant, Hamming};
 
-pub struct Database<V: Value> {
+pub struct DeletionDatabase<V> where V: Value + Window + DeletionVariant + Hamming {
     dimensions: usize,
     tolerance: usize,
     partition_count: usize,
@@ -18,11 +18,11 @@ pub struct Database<V: Value> {
 }
 
 
-impl<V: Value> DeletionDatabase<V> {
+impl<V: Value + Window + DeletionVariant + Hamming> DeletionDatabase<V> {
     /*
      * Partition the keyspace as evenly as possible
      */
-    pub fn new(dimensions: usize, tolerance: usize) -> Database<V> {
+    pub fn new(dimensions: usize, tolerance: usize) -> DeletionDatabase<V> {
 
         // Determine number of partitions
         let partition_count = if tolerance == 0 {
@@ -56,7 +56,7 @@ impl<V: Value> DeletionDatabase<V> {
         }
 
         // Done!
-        return Database {
+        return DeletionDatabase {
             dimensions: dimensions,
             tolerance: tolerance,
             partition_count: partition_count,
@@ -65,49 +65,6 @@ impl<V: Value> DeletionDatabase<V> {
     }
 
     pub fn get(&self, key: &V) -> Option<HashSet<V>> {
-        /*
-         * This is the method described in the HmSearch paper.  It's slower than
-         * just checking the hamming distance, but I'm going to leave it commented
-         * out here becase it may be necessary for vector-hamming distances (as
-         * opposed to scalar-hamming distances).
-         */
-        //let mut results: ResultAccumulator = ResultAccumulator::new(self.tolerance, key.clone());
-
-        //for partition in self.partitions.iter() {
-        //    let found = partition.find(key.clone());
-
-        //    for k in found.iter() {
-        //        results.merge(k);
-        //    }
-        //}
-
-        //return results.found_values()
-
-        let mut results: HashSet<V> = HashSet::new();
-
-        // Split across tasks?
-        for partition in self.partitions.iter() {
-            for result in partition.get(key).into_iter() {
-                match result {
-                    FindResult::ZeroVariant(value) => {
-                        if value.hamming(key) <= self.tolerance { 
-                            results.insert(value);
-                        };
-                    },
-                    FindResult::OneVariant(value) => {
-                        if value.hamming(key) <= self.tolerance {
-                            results.insert(value);
-                        };
-                    }
-                }
-            }
-        }
-
-        if results.is_empty() {
-            None
-        } else {
-            Some(results)
-        }
     }
 
     /*
@@ -141,6 +98,29 @@ impl<V: Value> DeletionDatabase<V> {
     }
 }
 
+impl<V: Value + Window + DeletionVariant + Hamming> fmt::Debug for DeletionDatabase<V> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "({}:{}:{})", self.dimensions, self.tolerance, self.partition_count)
+    }
+}
+
+impl<V: Value + Window + DeletionVariant + Hamming> PartialEq for DeletionDatabase<V> {
+    fn eq(&self, other: &DeletionDatabase<V>) -> bool {
+        return self.dimensions == other.dimensions &&
+            self.tolerance == other.tolerance &&
+            self.partition_count == other.partition_count;// &&
+            //self.partitions.eq(&other.partitions);
+    }
+
+    fn ne(&self, other: &DeletionDatabase<V>) -> bool {
+        return self.dimensions != other.dimensions ||
+            self.tolerance != other.tolerance ||
+            self.partition_count != other.partition_count; // ||
+            //self.partitions.eq(&other.partitions);
+    }
+}
+
+
 #[cfg(test)]
 mod test {
     extern crate rand;
@@ -148,13 +128,13 @@ mod test {
     use std::collections::{HashSet};
     use self::rand::{thread_rng, sample, Rng};
 
-    use db::database::Database;
+    use db::substitution_database::{DeletionDatabase};
     use db::partition::Partition;
 
     #[test]
     fn partition_evenly() {
-        let a: Database<usize> = Database::new(32, 5);
-        let b = Database {
+        let a: DeletionDatabase<usize> = DeletionDatabase::new(32, 5);
+        let b = DeletionDatabase {
             dimensions: 32,
             tolerance: 5,
             partition_count: 4,
@@ -170,8 +150,8 @@ mod test {
 
     #[test]
     fn partition_unevenly() {
-        let a: Database<usize> = Database::new(32, 7);
-        let b = Database {
+        let a: DeletionDatabase<usize> = DeletionDatabase::new(32, 7);
+        let b = DeletionDatabase {
             dimensions: 32,
             tolerance: 7,
             partition_count: 5,
@@ -188,8 +168,8 @@ mod test {
 
     #[test]
     fn partition_too_many() {
-        let a: Database<usize> = Database::new(4, 8);
-        let b = Database {
+        let a: DeletionDatabase<usize> = DeletionDatabase::new(4, 8);
+        let b = DeletionDatabase {
             dimensions: 4,
             tolerance: 8,
             partition_count: 3,
@@ -205,8 +185,8 @@ mod test {
 
     #[test]
     fn partition_zero() {
-        let a: Database<usize> = Database::new(32, 0);
-        let b = Database {
+        let a: DeletionDatabase<usize> = DeletionDatabase::new(32, 0);
+        let b = DeletionDatabase {
             dimensions: 32,
             tolerance: 0,
             partition_count: 1,
@@ -220,8 +200,8 @@ mod test {
 
     #[test]
     fn partition_with_no_bytes() {
-        let a: Database<usize> = Database::new(0, 0);
-        let b = Database {
+        let a: DeletionDatabase<usize> = DeletionDatabase::new(0, 0);
+        let b = DeletionDatabase {
             dimensions: 0,
             tolerance: 0,
             partition_count: 1,
@@ -235,7 +215,7 @@ mod test {
 
     #[test]
     fn find_missing_key() {
-        let p: Database<usize> = Database::new(8, 2);
+        let p: DeletionDatabase<usize> = DeletionDatabase::new(8, 2);
         let a = 0b11111111usize;
         let keys = p.get(&a);
 
@@ -244,7 +224,7 @@ mod test {
 
     #[test]
     fn insert_first_key() {
-        let mut p: Database<usize> = Database::new(8, 2);
+        let mut p: DeletionDatabase<usize> = DeletionDatabase::new(8, 2);
         let a = 0b11111111usize;
 
         assert!(p.insert(a.clone()));
@@ -252,7 +232,7 @@ mod test {
 
     #[test]
     fn insert_second_key() {
-        let mut p: Database<usize> = Database::new(8, 2);
+        let mut p: DeletionDatabase<usize> = DeletionDatabase::new(8, 2);
         let a = 0b11111111usize;
 
         p.insert(a.clone());
@@ -262,7 +242,7 @@ mod test {
 
     #[test]
     fn find_inserted_key() {
-        let mut p: Database<usize> = Database::new(8, 2);
+        let mut p: DeletionDatabase<usize> = DeletionDatabase::new(8, 2);
         let a = 0b11111111usize;
         let mut b: HashSet<usize> = HashSet::new();
         b.insert(a.clone());
@@ -276,7 +256,7 @@ mod test {
 
     #[test]
     fn find_permutations_of_inserted_key() {
-        let mut p: Database<usize> = Database::new(8, 2);
+        let mut p: DeletionDatabase<usize> = DeletionDatabase::new(8, 2);
         let a = 0b00001111usize;
         let b = 0b00000111usize;
         let mut c = HashSet::new();
@@ -291,7 +271,7 @@ mod test {
 
     #[test]
     fn find_permutations_of_multiple_similar_keys() {
-        let mut p: Database<usize> = Database::new(8, 4);
+        let mut p: DeletionDatabase<usize> = DeletionDatabase::new(8, 4);
         let a = 0b00000000usize;
         let b = 0b10000000usize;
         let c = 0b10000001usize;
@@ -323,7 +303,7 @@ mod test {
             .map(|i| sample(&mut rng2, 0..dimensions, i % max_hd));
 
         for start_dimensions in start_dimensions_seq.take(1000usize) {
-            let mut p: Database<usize> = Database::new(dimensions, max_hd);
+            let mut p: DeletionDatabase<usize> = DeletionDatabase::new(dimensions, max_hd);
             let a = 0b11111111usize;
 
             let mut b = a.clone();
@@ -356,7 +336,7 @@ mod test {
             .filter(|start_dimensions| start_dimensions.len() > max_hd);
 
         for start_dimensions in start_dimensions_seq.take(1000usize) {
-            let mut p: Database<usize> = Database::new(dimensions, max_hd);
+            let mut p: DeletionDatabase<usize> = DeletionDatabase::new(dimensions, max_hd);
             let a = 0b11111111usize;
 
             let mut b = a.clone();
@@ -377,7 +357,7 @@ mod test {
 
     #[test]
     fn remove_inserted_key() {
-        let mut p: Database<usize> = Database::new(8, 2);
+        let mut p: DeletionDatabase<usize> = DeletionDatabase::new(8, 2);
         let a = 0b00001111usize;
 
         p.insert(a.clone());
@@ -391,7 +371,7 @@ mod test {
 
     #[test]
     fn remove_missing_key() {
-        let mut p: Database<usize> = Database::new(8, 2);
+        let mut p: DeletionDatabase<usize> = DeletionDatabase::new(8, 2);
         let a = 0b00001111usize;
 
         assert!(!p.remove(&a));
@@ -407,7 +387,7 @@ mod test {
         // NOTE: we need a better way of coercing values - right now we only support
         // Vec<u8> - would be much better to implement a generic so we could set 
         // values directly.  IE, we need to convert u16 to [u8] here, and that's annoying
-        let mut p: Database<usize> = Database::new(16, 4);
+        let mut p: DeletionDatabase<usize> = DeletionDatabase::new(16, 4);
 
         let mut expected_present = [false; 65536];
         let mut expected_absent = [false; 65536];
@@ -459,26 +439,3 @@ mod test {
         }
     }
 }
-
-impl<V: Value> fmt::Debug for Database<V> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        write!(f, "({}:{}:{})", self.dimensions, self.tolerance, self.partition_count)
-    }
-}
-
-impl<V: Value> PartialEq for Database<V> {
-    fn eq(&self, other: &Database<V>) -> bool {
-        return self.dimensions == other.dimensions &&
-            self.tolerance == other.tolerance &&
-            self.partition_count == other.partition_count;// &&
-            //self.partitions.eq(&other.partitions);
-    }
-
-    fn ne(&self, other: &Database<V>) -> bool {
-        return self.dimensions != other.dimensions ||
-            self.tolerance != other.tolerance ||
-            self.partition_count != other.partition_count; // ||
-            //self.partitions.eq(&other.partitions);
-    }
-}
-
