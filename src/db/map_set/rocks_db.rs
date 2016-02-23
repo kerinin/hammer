8b2068b8f05a270397eadb3b92f1d894b8e400d3
Ryan Michael <kerinin@gmail.com>
2016-02-23 17:18:11 -0600

    RocksDB refactor and tests

--- src/db/map_set/rocks_db.rs
+++ src/db/map_set/rocks_db.rs
@@ -1,12 +1,15 @@
-use std::clone;
-use std::cmp;
-use std::hash;
 use std::env;
+use std::fs;
+use std::clone::Clone;
+use std::cmp::Eq;
+use std::hash::Hash;
+use std::ops::Drop;
+use std::fmt::Debug;
+use std::path::PathBuf;
 use std::marker::PhantomData;
 
 use std::collections::HashSet;
 
-use libc::c_int;
 use rocksdb::{DB, Writable, Options, Direction, IteratorMode};
 use rustc_serialize::{Encodable, Decodable};
 use bincode::SizeLimit;
@@ -15,28 +18,57 @@ use uuid::Uuid;
 
 use super::MapSet;
 
-pub type TempRocksDB<K, V> = RocksDB<K, V>;
+pub struct TempRocksDB<K, V> {
+    dir: PathBuf,
+    db: RocksDB<K, V>,
+}
 
 impl<K, V> TempRocksDB<K, V> {
-    pub fn temp_with_opts(opts: Options) -> TempRocksDB<K, V> {
+    pub fn with_opts(opts: Options) -> TempRocksDB<K, V> {
         let mut dir = env::temp_dir(); 
         dir.push(&Uuid::new_v4().to_hyphenated_string());
 
-        let db = DB::open(&opts, dir.to_str().unwrap()).unwrap();
-
-        RocksDB{
-            key: PhantomData,
-            value: PhantomData,
-            db: db,
+        TempRocksDB{
+            dir: dir.clone(),
+            db: RocksDB::with_opts(dir.to_str().unwrap(), opts),
         }
     }
 
-    pub fn new_temp() -> TempRocksDB<K, V> {
-        let mut opts = Options::new();
-        opts.add_comparator("prefix comparator", prefix_compare);
-        opts.create_if_missing(true);
+    pub fn new() -> TempRocksDB<K, V> {
+        let mut dir = env::temp_dir(); 
+        dir.push(&Uuid::new_v4().to_hyphenated_string());
 
-        TempRocksDB::temp_with_opts(opts)
+        TempRocksDB{
+            dir: dir.clone(),
+            db: RocksDB::new(dir.to_str().unwrap()),
+        }
+    }
+}
+
+impl<K, V> Drop for TempRocksDB<K, V> {
+    fn drop(&mut self) {
+        // Nothing we can do about it here, so ¯\_(ツ)_/¯
+        let _ = fs::remove_dir_all(self.dir.to_str().unwrap());
+    }
+}
+
+impl<K, V> MapSet for TempRocksDB<K, V>
+where   K: Debug + Clone + Eq + Hash + Encodable + Decodable,
+V: Debug + Clone + Eq + Hash + Encodable + Decodable,
+{
+    type Key = K;
+    type Value = V;
+
+    fn insert(&mut self, key: K, value: V) -> bool {
+        self.db.insert(key, value)
+    }
+
+    fn get(&self, key: &K) -> Option<HashSet<V>> {
+        self.db.get(key)
+    }
+
+    fn remove(&mut self, key: &K, value: &V) -> bool {
+        self.db.remove(key, value)
     }
 }
 
@@ -52,18 +84,6 @@ pub struct RocksDB<K, V> {
     db: DB,
 }
 
-fn prefix_compare(a: &[u8], b: &[u8]) -> c_int {
-    if a.len() < b.len() {
-        prefix_compare(a, &b[..a.len()])
-    } else if a < b {
-        1
-    } else if a > b {
-        -1
-    } else {
-        0
-    }
-}
-
 impl<K, V> RocksDB<K, V> {
     pub fn with_opts(path: &str, opts: Options) -> RocksDB<K, V> {
         let db = DB::open(&opts, path).unwrap();
@@ -76,27 +96,34 @@ impl<K, V> RocksDB<K, V> {
     }
 
     pub fn new(path: &str) -> RocksDB<K, V> {
-        let mut opts = Options::new();
-        opts.add_comparator("prefix comparator", prefix_compare);
+        let db = DB::open_default(path).unwrap();
 
-        RocksDB::with_opts(path, opts)
+        RocksDB{
+            key: PhantomData,
+            value: PhantomData,
+            db: db,
+        }
     }
 }
 
 
 impl<K, V> MapSet for RocksDB<K, V>
-where   K: clone::Clone + cmp::Eq + hash::Hash + Encodable + Decodable,
-V: clone::Clone + cmp::Eq + hash::Hash + Encodable + Decodable,
+where   K: Debug + Clone + Eq + Hash + Encodable + Decodable,
+V: Debug + Clone + Eq + Hash + Encodable + Decodable,
 {
     type Key = K;
     type Value = V;
 
     fn insert(&mut self, key: K, value: V) -> bool {
-        let encoded_key: Vec<u8> = encode(&(key, value), SizeLimit::Infinite).unwrap();
+        let encoded_key: Vec<u8> = encode(&(key.clone(), value.clone()), SizeLimit::Infinite).unwrap();
 
         match self.db.get(&encoded_key) {
-            Ok(Some(_)) => return false,
-            Err(e) => panic!(e),
+            Ok(Some(_)) => {
+                return false
+            },
+            Err(e) => {
+                panic!(e)
+            },
             Ok(None) => {
                 self.db.put(&encoded_key, &[]).unwrap();
                 true
@@ -110,7 +137,8 @@ V: clone::Clone + cmp::Eq + hash::Hash + Encodable + Decodable,
 
         for (k, _) in self.db.iterator(IteratorMode::From(&encoded_key_prefix, Direction::forward)) {
             let (decoded_key, decoded_value): (K, V) = decode(&k).unwrap();
-            if *key == decoded_key {
+
+            if *key != decoded_key {
                 break
             }
             out.insert(decoded_value);
@@ -144,20 +172,115 @@ mod test {
 
     use self::quickcheck::quickcheck;
 
-    use db::map_set::{MapSet, RocksDB};
+    use db::map_set::{MapSet, TempRocksDB};
 
     #[test]
     fn inserted_exists() {
-        fn prop(a: u64, b: u64, c: u64) -> quickcheck::TestResult {
-            let mut db = RocksDB::new_temp();
-            db.insert(a.clone(), b.clone());
-            db.insert(b.clone(), c.clone());
+        fn prop(k: u64, v: u64) -> quickcheck::TestResult {
+            let mut db = TempRocksDB::new();
+            db.insert(k.clone(), v.clone());
 
-            match db.get(&a) {
-                Some(results) => quickcheck::TestResult::from_bool(results.contains(&b)),
+            match db.get(&k) {
+                Some(results) => quickcheck::TestResult::from_bool(results.contains(&v)),
+                None => quickcheck::TestResult::failed(),
+            }
+        }
+        quickcheck(prop as fn(u64, u64) -> quickcheck::TestResult);
+    }
+
+    #[test]
+    fn not_inserted_no_exists() {
+        fn prop(k: u64, v1: u64, v2: u64) -> quickcheck::TestResult {
+            if v1 == v2 {
+                return quickcheck::TestResult::discard()
+            }
+
+            let mut db = TempRocksDB::new();
+            db.insert(k.clone(), v1.clone());
+
+            match db.get(&k) {
+                Some(results) => quickcheck::TestResult::from_bool(!results.contains(&v2)),
                 None => quickcheck::TestResult::failed(),
             }
         }
         quickcheck(prop as fn(u64, u64, u64) -> quickcheck::TestResult);
+    }
+
+    #[test]
+    fn key_not_inserted_no_exists() {
+        fn prop(k1: u64, k2: u64, v: u64) -> quickcheck::TestResult {
+            if k1 == k2 {
+                return quickcheck::TestResult::discard()
+            }
+
+            let mut db = TempRocksDB::new();
+            db.insert(k1.clone(), v.clone());
+
+            match db.get(&k2) {
+                Some(_) => quickcheck::TestResult::failed(),
+                None => quickcheck::TestResult::passed(),
+            }
+        }
+        quickcheck(prop as fn(u64, u64, u64) -> quickcheck::TestResult);
+    }
+
+    #[test]
+    fn deleted_no_exists() {
+        fn prop(k: u64, v1: u64, v2: u64) -> quickcheck::TestResult {
+            if v1 == v2 {
+                return quickcheck::TestResult::discard()
+            }
+
+            let mut db = TempRocksDB::new();
+            db.insert(k.clone(), v1.clone());
+            db.insert(k.clone(), v2.clone());
+            db.remove(&k, &v1);
+
+            match db.get(&k) {
+                Some(results) => quickcheck::TestResult::from_bool(!results.contains(&v1)),
+                None => quickcheck::TestResult::failed(),
+            }
+        }
+        quickcheck(prop as fn(u64, u64, u64) -> quickcheck::TestResult);
+    }
+
+    #[test]
+    fn not_deleted_exists() {
+        fn prop(k: u64, v1: u64, v2: u64) -> quickcheck::TestResult {
+            if v1 == v2 {
+                return quickcheck::TestResult::discard()
+            }
+
+            let mut db = TempRocksDB::new();
+            db.insert(k.clone(), v1.clone());
+            db.insert(k.clone(), v2.clone());
+            db.remove(&k, &v1);
+
+            match db.get(&k) {
+                Some(results) => quickcheck::TestResult::from_bool(results.contains(&v2)),
+                None => quickcheck::TestResult::failed(),
+            }
+        }
+        quickcheck(prop as fn(u64, u64, u64) -> quickcheck::TestResult);
+    }
+
+    #[test]
+    fn key_deleted_no_exists() {
+        fn prop(k1: u64, k2: u64, v1: u64, v2: u64) -> quickcheck::TestResult {
+            if k1 == k2 {
+                return quickcheck::TestResult::discard()
+            }
+
+            let mut db = TempRocksDB::new();
+            db.insert(k1.clone(), v1.clone());
+            db.insert(k2.clone(), v2.clone());
+            db.remove(&k1, &v1);
+
+            match db.get(&k1) {
+                Some(_) => quickcheck::TestResult::failed(),
+                None => quickcheck::TestResult::passed(),
+            }
+        }
+        quickcheck(prop as fn(u64, u64, u64, u64) -> quickcheck::TestResult);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 extern crate rocksdb;
 extern crate rustc_serialize;
 extern crate bincode;
-extern crate libc;
 extern crate uuid;
 
 // pub mod bit_matrix;
