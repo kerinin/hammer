c79b47a0d13ce908cbfe38945fc56801a92f1bbf
Ryan Michael <kerinin@gmail.com>
2016-02-22 23:51:54 -0600

    First iteraton of RocksDB

--- src/db/map_set.rs
+++ src/db/map_set.rs
@@ -28,9 +28,16 @@
 use std::clone;
 use std::cmp;
 use std::hash;
+use std::marker::PhantomData;
 
 use std::collections::*;
 use std::collections::hash_map::Entry::{Vacant, Occupied};
+
+use libc::c_int;
+use rocksdb::*;
+use rustc_serialize::*;
+use bincode::SizeLimit;
+use bincode::rustc_serialize::{encode, decode};
 
 pub trait MapSet: Sized {
     type Key: clone::Clone + cmp::Eq + hash::Hash;
@@ -38,8 +45,100 @@ pub trait MapSet: Sized {
 
     fn new() -> Self;
     fn insert(&mut self, key: Self::Key, value: Self::Value) -> bool;
-    fn get(&self, key: &Self::Key) -> Option<&HashSet<Self::Value>>;
+    fn get(&self, key: &Self::Key) -> Option<HashSet<Self::Value>>;
     fn remove(&mut self, key: &Self::Key, value: &Self::Value) -> bool;
+}
+
+/// RocksDBMapSet uses RocksDB to store a mapping from keys to sets of values
+///
+/// Internally, k/v paris are mapped to binary RocksDB keys.  Sets of values are
+/// retrieved by scanning RocksDB keys whose prefix match the given key and 
+/// reconstructing the value from the "end" of the RocksDB key
+///
+pub struct RocksDBMapSet<K, V> {
+    key: PhantomData<K>,
+    value: PhantomData<V>,
+    db: DB,
+}
+
+fn prefix_compare(a: &[u8], b: &[u8]) -> c_int {
+    if a.len() < b.len() {
+        prefix_compare(a, &b[..a.len()])
+    } else if a < b {
+        1
+    } else if a > b {
+        -1
+    } else {
+        0
+    }
+}
+
+impl<K, V> MapSet for RocksDBMapSet<K, V>
+where   K: clone::Clone + cmp::Eq + hash::Hash + Encodable + Decodable,
+V: clone::Clone + cmp::Eq + hash::Hash + Encodable + Decodable,
+{
+    type Key = K;
+    type Value = V;
+
+    fn new() -> RocksDBMapSet<K, V> {
+        let path = "path/to/rocksdb";
+        let mut opts = Options::new();
+        opts.add_comparator("prefix comparator", prefix_compare);
+
+        let db = DB::open(&opts, path).unwrap();
+
+        RocksDBMapSet{
+            key: PhantomData,
+            value: PhantomData,
+            db: db,
+        }
+    }
+
+    fn insert(&mut self, key: K, value: V) -> bool {
+        let encoded_key: Vec<u8> = encode(&(key, value), SizeLimit::Infinite).unwrap();
+
+        match self.db.get(&encoded_key) {
+            Ok(Some(_)) => return false,
+            Err(e) => panic!(e),
+            Ok(None) => {
+                self.db.put(&encoded_key, &[]).unwrap();
+                true
+            },
+        }
+    }
+
+    fn get(&self, key: &K) -> Option<HashSet<V>> {
+        let mut out = HashSet::new();
+        let encoded_key_prefix: Vec<u8> = encode(&key, SizeLimit::Infinite).unwrap();
+
+        for (k, _) in self.db.iterator(IteratorMode::From(&encoded_key_prefix, Direction::forward)) {
+            let (decoded_key, decoded_value): (K, V) = decode(&k).unwrap();
+            if *key == decoded_key {
+                break
+            }
+            out.insert(decoded_value);
+        }
+
+        if out.is_empty() {
+            None
+        } else {
+            // Some(&out)
+            None
+        }
+    }
+
+    fn remove(&mut self, key: &K, value: &V) -> bool {
+        let encoded_key: Vec<u8> = encode(&(key, value), SizeLimit::Infinite).unwrap();
+
+        match self.db.get(&encoded_key) {
+            Err(e) => panic!(e),
+            Ok(None) => return false,
+            Ok(Some(_)) => {
+                self.db.delete(&encoded_key).unwrap();
+                true
+            }
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -75,8 +174,11 @@ where   K: clone::Clone + cmp::Eq + hash::Hash,
         }
     }
 
-    fn get(&self, key: &K) -> Option<&HashSet<V>> {
-        return self.data.get(key);
+    fn get(&self, key: &K) -> Option<HashSet<V>> {
+        match self.data.get(key) {
+            Some(h) => Some(h.clone()),
+            None => None,
+        }
     }
 
     fn remove(&mut self, key: &K, value: &V) -> bool {
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,7 @@
+extern crate rocksdb;
+extern crate rustc_serialize;
+extern crate bincode;
+extern crate libc;
+
 // pub mod bit_matrix;
 pub mod db;
